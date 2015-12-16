68329f0d064740d9b6b177aa53b89597421e6a51
Marcel Tyszkiewicz <marcel.tyszkiewicz@gmail.com>
2015-12-16 12:33:16 +0000

    Initial commit

---
+++ src/app/main/main.controller.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('LandApp')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController() {
+  }
+})();
