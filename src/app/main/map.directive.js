acfe7cd63eaffd8cd7e04ef5e700586d9ee43cef
Marcel Tyszkiewicz <marcel.tyszkiewicz@gmail.com>
2015-12-18 15:26:07 +0000

    Map layer definitions

--- src/app/main/map.directive.js
+++ src/app/main/map.directive.js
@@ -16,46 +16,58 @@
       bindToController: true
     };
 
-    var view,
-        map;
+    var vm,
+        that = {};
 
     return directive;
 
     /** @ngInject */
-    function MapController($rootScope, $log, mapService) {
-      var vm = this;
-      vm.mapService = mapService;
+    function MapController($rootScope, $scope, $log, mapService) {
+      vm = this;
+      that.scope = $scope;
+      that.mapService = mapService;
 
-      createMap(vm);
+      createMap();
+      mapService.addBaseMaps(that.map);
 
-      mapService.addBaseMaps(map);
+      watchFarmLayerChange();
+
+      $log.log(that.mapService.map.enabledFarmLayers);
     }
 
-    function createMap(vm) {
+    function watchFarmLayerChange() {
+      var foo = that.mapService;
+
+      that.scope.$watchCollection('that.mapService.map.enabledFarmLayers', function(newLayers, oldLayers) {
+        console.log(that.mapService.map.enabledFarmLayers, newLayers, oldLayers);
+      });
+    }
+
+    function createMap() {
       var timsFarm = ol.proj.fromLonLat([-0.658493, 51.191286]);
       var jamesFarm = ol.proj.fromLonLat([-1.315305, 51.324901]);
 
-      view = new ol.View({
-          center: jamesFarm,
-          maxZoom: 20,
-          minZoom: 7,
-          zoom: 13
+      that.view = new ol.View({
+        center: jamesFarm,
+        maxZoom: 20,
+        minZoom: 7,
+        zoom: 13
       });
 
       vm.zoomIn = function() {
-          view.setZoom(view.getZoom() + 1);
+        that.view.setZoom(that.view.getZoom() + 1);
       };
 
       vm.zoomOut = function() {
-          view.setZoom(view.getZoom() - 1);
+        that.view.setZoom(that.view.getZoom() - 1);
       };
 
-      map = new ol.Map({
-          target: 'map',
-          layers: [], // initialise without any layers because land-app-map-controller will set basemap
-          loadTilesWhileAnimating: true,
-          view: view,
-          controls: []
+      that.map = new ol.Map({
+        target: 'map',
+        layers: [], // initialise without any layers because land-app-map-controller will set basemap
+        loadTilesWhileAnimating: true,
+        view: that.view,
+        controls: []
       });
 
       //this.scope.map.addInteraction(clickHandler);
