bdd2ee58513bf83dd0f643762d0e31e7240f9bec
Sebastian Ovide <Sebastian.Ovide@geovation.uk>
2016-01-11 15:49:43 +0000

    added google analytics

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -6,9 +6,17 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($rootScope, $log, ENV) {
+  function runBlock($rootScope, $log, ENV, ga, firebaseService) {
     $rootScope.ENV = ENV;
+    firebaseService.firebaseRef.onAuth(setGAUserID);
+
     $log.debug('runBlock end');
+
+    //////////
+    function setGAUserID(authData) {
+      $log.debug("GA userId : " + authData.uid);
+      ga('set', 'userId', authData.uid);
+    }
   }
 
 })();
