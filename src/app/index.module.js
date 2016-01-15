0d59782aa8242876ab892d58ec92ca0cdc03c5cc
Marcel Tyszkiewicz <marcel.tyszkiewicz@gmail.com>
2016-01-15 14:33:07 +0000

    Editable feature title

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -3,8 +3,13 @@
 
   angular
     .module('LandApp', ['ngResource', 'ngRoute', 'ngMaterial', 'firebase', 'xeditable'])
-    .run(function(editableOptions) {
-      editableOptions.theme = 'default';
+    .run(function(editableThemes) {
+      editableThemes.default.submitTpl = '<button type="submit"><i class="fa fa-check"></i></button>';
+      editableThemes.default.cancelTpl = '<button type="button" ng-click="$form.$cancel()"><i class="fa fa-times"></i></button>';
+      editableThemes.default.formTpl = '<form class="editable-wrap md-toolbar-tools"></form>';
+      editableThemes.default.inputTpl = '<md-input-container></md-input-container>';
+
+      console.log(editableThemes);
     });
 
 })();
