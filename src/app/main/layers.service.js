de4b4103e6ba8fb6e46bce704fc31a021312d370
Marcel Tyszkiewicz <marcel.tyszkiewicz@gmail.com>
2016-01-21 15:26:47 +0000

    Code cleanup

--- src/app/main/layers.service.js
+++ src/app/main/layers.service.js
@@ -19,67 +19,64 @@
     //////////
     function createEnvironmentalLayers() {
       return [{
-          name: 'AONB',
-          type: 'wms',
-          url: "https://www.geostore.com/OGC/OGCInterface?UID=UDATAGOV2011&PASSWORD=datagov2011&INTERFACE=ENVIRONMENT&LC=2000000000000040000000000000000000000000000000000001040000000000000000",
-          layers: 'eainspire2011-wms-areas_of_onb_inspire'
-        }, {
-          name: 'Ancient Woodland',
-          type: 'wms',
-          url: "https://www.geostore.com/OGC/OGCInterface?UID=UDATAGOV2011&PASSWORD=datagov2011&INTERFACE=ENVIRONMENT&LC=2000000000000040000000000000000000000000000000000001040000000000000000",
-          layers: 'eainspire2011-wms-ancient_woodlandv_inspire'
-        }, {
-          name: 'SAC',
-          type: 'wms',
-          url: "https://www.geostore.com/OGC/OGCInterface?UID=UDATAGOV2011&PASSWORD=datagov2011&INTERFACE=ENVIRONMENT&LC=2000000000000000000",
-          layers: 'eainspire2011-wms-special_area_of_conservation_inspire'
-        }, {
-          name: 'SSSI',
-          type: 'wms',
-          url: "https://www.geostore.com/OGC/OGCInterface?UID=UDATAGOV2011&PASSWORD=datagov2011&INTERFACE=ENVIRONMENT&LC=2000000000000040000000000000000000000000000000000001040000000000000000",
-          layers: 'eainspire2011-wms-sites_of_ssi_inspire'
-        }];
+        name: 'AONB',
+        type: 'wms',
+        url: "https://www.geostore.com/OGC/OGCInterface?UID=UDATAGOV2011&PASSWORD=datagov2011&INTERFACE=ENVIRONMENT&LC=2000000000000040000000000000000000000000000000000001040000000000000000",
+        layers: 'eainspire2011-wms-areas_of_onb_inspire'
+      }, {
+        name: 'Ancient Woodland',
+        type: 'wms',
+        url: "https://www.geostore.com/OGC/OGCInterface?UID=UDATAGOV2011&PASSWORD=datagov2011&INTERFACE=ENVIRONMENT&LC=2000000000000040000000000000000000000000000000000001040000000000000000",
+        layers: 'eainspire2011-wms-ancient_woodlandv_inspire'
+      }, {
+        name: 'SAC',
+        type: 'wms',
+        url: "https://www.geostore.com/OGC/OGCInterface?UID=UDATAGOV2011&PASSWORD=datagov2011&INTERFACE=ENVIRONMENT&LC=2000000000000000000",
+        layers: 'eainspire2011-wms-special_area_of_conservation_inspire'
+      }, {
+        name: 'SSSI',
+        type: 'wms',
+        url: "https://www.geostore.com/OGC/OGCInterface?UID=UDATAGOV2011&PASSWORD=datagov2011&INTERFACE=ENVIRONMENT&LC=2000000000000040000000000000000000000000000000000001040000000000000000",
+        layers: 'eainspire2011-wms-sites_of_ssi_inspire'
+      }];
     }
 
     function createBaseMapLayers() {
       return [{
-          name: 'Open Street Map',
-          type: 'base.osm'
-        }, {
-          name: 'Map Quest',
-          type: 'base.mapquest'
-        }, {
-          name: 'Aerial',
-          type: 'base.mapbox',
-          url: "https://api.tiles.mapbox.com/v4/"+ ENV.mapboxMapId +"/{z}/{x}/{y}.png?access_token=" + ENV.mapboxToken
-        }, {
-          name: 'Ordnance Survey',
-          disabled: true
-        }
-      ];
+        name: 'Open Street Map',
+        type: 'base.osm'
+      }, {
+        name: 'Map Quest',
+        type: 'base.mapquest'
+      }, {
+        name: 'Aerial',
+        type: 'base.mapbox',
+        url: "https://api.tiles.mapbox.com/v4/"+ ENV.mapboxMapId +"/{z}/{x}/{y}.png?access_token=" + ENV.mapboxToken
+      }, {
+        name: 'Ordnance Survey',
+        disabled: true
+      }];
     }
 
     function createFarmLayers() {
-      return [
-        {
-          name: 'RLR Parcel',
-          type: 'vector',
-          url: "/assets/geojson/watership_down_rpa.geojson",
-          fillColor: "rgba(255, 165, 0, 0.5)",
-          strokeColor: "rgba(255, 165, 0, 1)",
-        }, {
-          name: 'RLR PIF',
-          type: 'vector',
-          // url: "/data/geojson/land_registry_boundaries.geojson",
-          url: "/assets/geojson/watership_down_pif.geojson",
-          fillColor: "rgba(176, 23, 21, 0.5)",
-          strokeColor: "rgba(176, 23, 21, 1)",
-        }, {
-          name: 'LR Vectors',
-          type: 'vectorspace',
-          url: "https://api.vectorspace.io/spaces/27085816987650/layers/27790585888771/features?key=" + ENV.vectorspaceKey
-        }
-      ];
+      return [{
+        name: 'RLR Parcel',
+        type: 'vector',
+        url: "/assets/geojson/watership_down_rpa.geojson",
+        fillColor: "rgba(255, 165, 0, 0.5)",
+        strokeColor: "rgba(255, 165, 0, 1)",
+      }, {
+        name: 'RLR PIF',
+        type: 'vector',
+        // url: "/data/geojson/land_registry_boundaries.geojson",
+        url: "/assets/geojson/watership_down_pif.geojson",
+        fillColor: "rgba(176, 23, 21, 0.5)",
+        strokeColor: "rgba(176, 23, 21, 1)",
+      }, {
+        name: 'LR Vectors',
+        type: 'vectorspace',
+        url: "https://api.vectorspace.io/spaces/27085816987650/layers/27790585888771/features?key=" + ENV.vectorspaceKey
+      }];
     }
 
     function createDrawingTools() {
