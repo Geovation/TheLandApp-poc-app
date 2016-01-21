ef5093688982cd26951c9d15a2b3ae22418761d4
Marcel Tyszkiewicz <marcel.tyszkiewicz@gmail.com>
2016-01-21 14:39:34 +0000

    Filters for formatting area and length of features

---
+++ src/app/index.filters.js
@@ -0,0 +1,30 @@
+angular
+  .module("landAppFilters", [])
+  .filter("formatArea", formatArea)
+  .filter("formatLength", formatLength);
+
+function formatArea($sce) {
+  return function(area) {
+    if (area > 10000) {
+      area = Math.round(area / 1000000 * 100) / 100 + " km";
+    } else {
+      area = Math.round(area * 100) / 100 + " m";
+    }
+
+    area += "<sup>2</sup>";
+
+    return $sce.trustAsHtml(area);
+  };
+}
+
+function formatLength() {
+  return function(length) {
+    if (length > 1000) {
+      length = Math.round(length / 1000 * 100) / 100 + " km";
+    } else {
+      length = Math.round(length * 100) / 100 + " m";
+    }
+
+    return length;
+  };
+}
