3b55dd6280b5007098494d20b018b1779cb52601
Marcel Tyszkiewicz <marcel.tyszkiewicz@gmail.com>
2016-02-05 13:53:56 +0000

    New services for interacting with layers and firebase

---
+++ src/app/main/shared-components/firebase-layer.service.js
@@ -0,0 +1,59 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('LandApp')
+    .factory('firebaseLayerService', firebaseLayerService);
+
+  /** @ngInject */
+  function firebaseLayerService(firebaseReferenceService, layerDefinitionsService) {
+    var service = {
+      saveFarmLayers: saveFarmLayers,
+      saveDrawingLayers: saveDrawingLayers
+    };
+
+    return service;
+
+    //////////////// PUBLIC ////////////////
+
+    function saveFarmLayers(layerName) {
+      var layers = layerDefinitionsService.farmLayers;
+
+      if (layerName) {
+        layers = findLayersByName(layerName, layers);
+      }
+
+      saveLayers(layers);
+    }
+
+    function saveDrawingLayers(layerName) {
+      var layers = layerDefinitionsService.drawingLayers;
+
+      if (layerName) {
+        layers = findLayersByName(layerName, layers);
+      }
+
+      saveLayers(layers);
+    }
+
+    //////////////// PRIVATE ////////////////
+
+    function findLayersByName(layerName, layerList) {
+      return layerList.filter(function(layer) {
+        return layerName === layer.name;
+      });
+    }
+
+    function saveLayers(layersList) {
+      var payload = {};
+      var format = new ol.format.GeoJSON();
+
+      layersList.forEach(function(layer) {
+        payload[layer.name] = angular.copy(format.writeFeaturesObject(layer.olLayer.getSource().getFeatures()));
+      });
+
+      firebaseReferenceService.getUserLayersRef().update(payload);
+    }
+  }
+
+})();
---
+++ src/app/shared/firebase-reference.service.js
@@ -0,0 +1,39 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('LandApp')
+    .factory('firebaseReferenceService', firebaseReferenceService);
+
+  /** @ngInject */
+  function firebaseReferenceService(Firebase, ENV) {
+    var firebaseRef = new Firebase("https://" + ENV.firebase + ".firebaseio.com");
+    var service = {
+      ref: firebaseRef,
+      getUserInfoRef: getUserInfoRef,
+      getUserLayersRef: getUserLayersRef
+    };
+
+    return service;
+    ////////// public functions //////////////
+
+    function getUserInfoRef() {
+      return getUserUIDRef()
+        .child("info");
+    }
+
+    function getUserLayersRef() {
+      return getUserUIDRef()
+        .child("layers");
+    }
+
+    //////////// privates ////////////////
+    function getUserUIDRef() {
+      var uid = firebaseRef.getAuth().uid;
+      return firebaseRef
+        .child("users")
+        .child(uid);
+    }
+  }
+
+})();
