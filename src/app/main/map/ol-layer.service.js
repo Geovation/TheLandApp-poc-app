3441f63e1506ed909528b7a997abc73aa3fef5b8
Sebastian Ovide <Sebastian.Ovide@geovation.uk>
2016-02-17 08:15:14 +0000

    deleted custom-layers and layers-interactions

--- src/app/main/map/ol-layer.service.js
+++ src/app/main/map/ol-layer.service.js
@@ -6,7 +6,7 @@
     .factory('olLayerService', olLayerService);
 
   /** @ngInject */
-  function olLayerService(ol, $log, customLayerService, layerInteractionsService, LAYERS_Z_INDEXES) {
+  function olLayerService(ol, $log, LAYERS_Z_INDEXES) {
     var service = {
       buildLayerAndInteractions: buildLayerAndInteractions
     };
@@ -81,8 +81,8 @@
             });
             break;
           case 'vectorspace':
-            layer.olLayer = customLayerService.buildVectorSpace(layer);
-            layer.olMapInteractions = layerInteractionsService.buildVectorSpace(layer);
+            layer.olLayer = _buildVectorSpaceOlLayer(layer);
+            layer.olMapInteractions = _buildVectorSpaceOlMapInteractions(layer);
             break;
           default:
             $log.debug("layer type '" + JSON.stringify(layer.type) + "' not defined");
@@ -94,6 +94,66 @@
       return [new ol.Attribution({
         html: (attributionHtml || ('All maps &copy; <a href="https://geovation.uk/">Geovation</a>'))
       })];
+    }
+
+    function _buildVectorSpaceOlLayer(layer) {
+      var newLayer = new ol.layer.Vector({
+        zIndex: LAYERS_Z_INDEXES.external,
+        maxResolution: 5,
+        source: new ol.source.Vector({
+          attributions: [new ol.Attribution({
+            html: 'LR © ' + '<a href="http://vectorspace.io/">VectorSpace</a>'
+          })],
+          format: new ol.format.GeoJSON(),
+          strategy: ol.loadingstrategy.bbox,
+          loader: function (extent, resolution, projection) {
+            // wgs84
+            extent = ol.proj.transformExtent(extent, "EPSG:3857", "EPSG:4326");
+
+            $http.get(layer.url, {
+              headers : {
+                  "Accept": "application/json;srid=4326"
+              },
+              params : {
+                "bbox" : extent.join() + ",4326"
+              }
+            })
+            .then(function successCallback(response) {
+              if (response.data) {
+                var features = (new ol.format.GeoJSON())
+                  .readFeatures(response.data, {
+                      dataProjection: "EPSG:4326",
+                      featureProjection: projection
+                    });
+                var src = newLayer.getSource();
+                src.addFeatures(features);
+                $log.debug('new featurecount:', src.getFeatures().length);
+              }
+            }, function errorCallback(response) {
+              $log.debug(response);
+            });
+          }
+        })
+      });
+
+      return newLayer;
+    } // _buildVectorSpaceOlLayer
+
+    function _buildVectorSpaceOlMapInteractions(layer) {
+      var click = new ol.interaction.Select({
+        condition: function (e) {
+          return ol.events.condition.click(e) && !drawingToolsService.isAnyDrawingToolActive();
+        },
+        layers: [layer.olLayer]
+      });
+
+      click.on('select', function() {
+        $timeout(function() {
+          $rootScope.$broadcast('land-registry-features-selected', click.getFeatures().getArray());
+        });
+      });
+
+      return [click];
     }
   }
 })();
