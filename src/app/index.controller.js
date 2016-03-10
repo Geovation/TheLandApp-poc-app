19a6b4f13a81d7be3c431b21b7d4da9645dfacbf
Sebastian Ovide <Sebastian.Ovide@geovation.uk>
2016-03-10 16:59:19 +0000

    code cleaning

--- src/app/index.controller.js
+++ src/app/index.controller.js
@@ -25,7 +25,6 @@
       $window.location.reload();
     }
 
-
     function _healUserData(authData) {
       vm.authData = authData;
 
@@ -37,47 +36,16 @@
             logout();
           });
       }
-    }
 
-    function _writeUserEmail(authData) {
-      var deferred = $q.defer();
+      function _writeUserEmail() {
+        return firebaseReferenceService.getUserInfoRef()
+          .update({"email": authData.password.email});
+      }
 
-      firebaseReferenceService.getUserInfoRef()
-        .update({"email": authData.password.email },
-        function onComplete(error){
-          // it failed to write. Needs to sign in again.
-          if (error) {
-            deferred.reject(authData);
-          } else {
-            deferred.resolve(authData);
-          }
-        }
-      );
-
-      deferred.resolve(authData);
-      return deferred.promise;
-    }
-
-    function _saveUserConnectedTime(authData) {
-      var deferred = $q.defer();
-
-      // TODO: can I replace it with
-      // return firebaseReferenceService.getUserInfoRef()
-      // .update({"connectedAt": Firebase.ServerValue.TIMESTAMP });
-
-      firebaseReferenceService.getUserInfoRef()
-        .update({"connectedAt": Firebase.ServerValue.TIMESTAMP },
-          function onComplete(error){
-            // it failed to write. Needs to sign in again.
-            if (error) {
-              deferred.reject(authData);
-            } else {
-              deferred.resolve(authData);
-            }
-          }
-        );
-
-      return deferred.promise;
-    }
+      function _saveUserConnectedTime() {
+        return firebaseReferenceService.getUserInfoRef()
+         .update({"connectedAt": Firebase.ServerValue.TIMESTAMP });
+      }
+    } //_healUserData
   }
 })();
