881d86134d7e039db3b2291262771298dfbc31b3
Marcel Tyszkiewicz <marcel.tyszkiewicz@gmail.com>
2016-04-05 09:34:43 +0100

    Comment update

--- src/app/main/shared-components/ol-layer-group.service.js
+++ src/app/main/shared-components/ol-layer-group.service.js
@@ -100,7 +100,9 @@
         _groupCollection[groupName].setVisible(isVisible);
       }
 
-      // when the myFarm project is toggled, hide all of its farm layers
+      // because we are overriding the control for farm layers
+      // by manually toggling them, they need to be explicitly hidden
+      // (group visibility doesn't override layer visibility)
       if (groupName === "myFarm") {
         angular.forEach(_layerDefinitions.myFarm.farmLayers, function(layer) {
           layer.olLayer.setVisible(false);
