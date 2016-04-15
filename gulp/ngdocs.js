5a78159022ea1ea01f757ec309b5b66278388ed1
Marcel Tyszkiewicz <marcel.tyszkiewicz@gmail.com>
2016-04-15 17:54:25 +0100

    ngdoc generators

---
+++ gulp/ngdocs.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var gulp = require('gulp');
+var recursiveFolder = require('gulp-recursive-folder');
+var gulpDocs = require('gulp-ngdocs');
+
+var options = {
+  scripts: [
+    'bower_components/angular/angular.min.js',
+    'bower_components/angular/angular.min.js.map',
+    'bower_components/angular-animate/angular-animate.min.js',
+    'bower_components/angular-animate/angular-animate.min.js.map'
+  ]
+};
+
+gulp.task('ngdocs', recursiveFolder('src', function(foundFolder) {
+  return gulp.src(foundFolder.path + "/*.js")
+    .pipe(gulpDocs.process(options))
+    .pipe(gulp.dest('./docs'));
+}));
